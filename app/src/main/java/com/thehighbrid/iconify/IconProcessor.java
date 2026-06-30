f0a1cb19dc9f2f15c0481cc102a3a3dbe8759f59
TheHighBrid <120822557+TheHighBrid@users.noreply.github.com>
2026-06-30 12:01:46 -0400

    Expand processor imports

--- app/src/main/java/com/thehighbrid/iconify/IconProcessor.java
+++ app/src/main/java/com/thehighbrid/iconify/IconProcessor.java
@@ -1,5 +1,11 @@
 package com.thehighbrid.iconify;
 
+import android.graphics.Bitmap;
+
 public final class IconProcessor {
     private IconProcessor() {}
+
+    public static Bitmap render(Bitmap input) {
+        return input;
+    }
 }
